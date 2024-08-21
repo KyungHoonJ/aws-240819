d679d9702ca59f5f7a38f2c83a016bad757cbc30
KyungHoonJ <rudekrudek@naver.com>
2024-08-21 13:46:30 +0900

    feat: get width

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,8 +1,15 @@
 import TodoList from "./components/TodoList";
 
+const GetWidth = (): JSX.Element => {
+  const width = document.body.clientWidth;
+
+  return <h1>{width}</h1>;
+};
+
 function App() {
   return (
     <div className="App">
+      <GetWidth />
       <TodoList />
     </div>
   );
